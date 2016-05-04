bd822d180f22e872f8ed537524ecbce02ed67edb
Quentin Gérard <qgerard.gerard@gmail.com>
2016-05-04 13:28:02 +0200

    To be deleted

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var Recaptcha = require('recaptcha').Recaptcha;
+
+var PUBLIC_KEY = '6LcLDB8TAAAAAKrfEiNeF2iq5RMlmdU4widRMBxn';
+var PRIVATE_KEY = '6LcLDB8TAAAAAAGpQObES-oduB7dN8aXkWKj2yk0';
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,16 +27,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/contact', function(req, res){
+  var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY);
+
+  res.render('contact.jade', {
+    layout: false,
+    locals: {
+      recaptcha_form: recaptcha.toHTML()
+    }
+  });
+});
+
 // MUST be after bodyParser have been declared
 app.post('/contact', function(req,res){
+  var data = {
+    remoteip: req.connection.remoteAddress,
+    challenge: req.body.recaptcha_challenge_field,
+    response: req.body.recapthca_response_field
+  };
+  var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
+
   var mailOpts, smtpTrans;
 
+  recaptcha.verify(function(success, error_code){
+    if(success){
+      res.send('Recaptcha response valid.');
+    }
+    else {
+      // Redisplay the form.
+      res.render('contact.jade', {
+        layout: false,
+        locals: {
+          recaptcha_form: recaptcha.toHTML()
+        }
+      });
+    }
+  });
 
   mailOpts = {
-    from: req.body.username + ' <' + req.body.email + '>',
+    from: 'qgerard.gerard@gmail.com',
     to: 'quentin@realtelematics.co.za',
     subject: 'Website contact',
-    text: req.body.message
+    text: req.body.message,
+    html: '<h1>Sender Name:</h1><h3>' + req.body.username + '</h3><h1>Email Address:</h1><h3>' + req.body.email + '</h3><p>' + req.body.message + '</p>'
   };
 
   smtpTrans = nodemailer.createTransport("SMTP",{
@@ -45,14 +82,15 @@ app.post('/contact', function(req,res){
 
   smtpTrans.sendMail(mailOpts, function(error, message){
     if(error){
+          res.render('contact', {title: 'Contact', status: 'Error sending message!'});
           console.log(error);
       }else{
+          console.log(req.body.username + ' <' + req.body.email + '>');
+          res.render('contact', {title: 'Contact', status: 'Message sent!'});
           console.log("Message sent");
         }
 
   });
-    console.log(req.body.username + ' <' + req.body.email + '>');
-    res.render('contact', {title: 'Thanks'});
     smtpTrans.close();
 });
 
